77f4b40961ec114bac777b011dbe10679643ea4f
ImLvna <44179559+ImLvna@users.noreply.github.com>
2022-02-03 18:18:32 -0500

    wtf why no workie

--- infiniteRoyalGems.lua
+++ infiniteRoyalGems.lua
@@ -4,9 +4,9 @@ print('15s up')
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
         if syn~=nil then 
-            syn.queue_on_teleport(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))
+            syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))()")
         else 
-            queue_on_teleport(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))
+            queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))()")
         end
     end
 end)
