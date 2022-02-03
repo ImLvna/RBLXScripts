3488da35e65bc8cd54fb9082e227cae4dccb59b8
ImLvna <44179559+ImLvna@users.noreply.github.com>
2022-02-03 17:58:09 -0500

    oops forgot to compare to nil (i dont code in lua often (like ever))

--- infiniteRoyalGems.lua
+++ infiniteRoyalGems.lua
@@ -1,6 +1,6 @@
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
-        if syn.queue_on_teleport then 
+        if syn.queue_on_teleport~=nil then 
             syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))()")
         else 
             queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ImLvna/RBLXScripts/main/infiniteRoyalGems.lua', true))()")
